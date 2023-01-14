5ac52bb5e2240124c55d1d93a856099509c0b9d0
Abhinav Kumar <abhinav.ai.ml@gmail.com>
2023-01-15 01:51:29 +0530

    "initial commit"

---
+++ mongtest.py
@@ -0,0 +1,16 @@
+import pymongo
+
+client = pymongo.MongoClient(
+    "mongodb+srv://abhinav5600:abhinav5600@cluster0.z5okehm.mongodb.net/?retryWrites=true&w=majority")
+db = client.test
+print(db)
+
+d = {
+    "name": "Abhinav",
+    "surname": "Kumar",
+    "email": "abhinav@gmail.com"
+}
+
+db1 = client['mongotest']
+coll = db['test']
+coll.insert_one(d)
