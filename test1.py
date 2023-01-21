2bfad4370a0a6e97b7bdf1ea6d310dfa50c990b9
Abhinav Kumar <abhinav.ai.ml@gmail.com>
2023-01-21 14:54:45 +0530

    adding more queries.

--- test1.py
+++ test1.py
@@ -1,1 +1,23 @@
-print("This is for testing for purpose")
+import pymongo
+
+client = pymongo.MongoClient("mongodb+srv://abhinav5600:abhik5600@cluster0.z5okehm.mongodb.net/?retryWrites=true&w=majority")
+db =client.test
+
+database = client['myinfo']
+collection = db['user']
+
+# record = collection.find()
+# for i in record:
+    # print(i)
+
+# record = collection.find({'companyName': 'iNeuron' })
+# for i in record:
+#     print(i)
+
+# record = collection.find({'courseOffered':'Machine Learning with Deployment' })
+# for i in record:
+#     print(i)
+
+record = collection.find({'courseOffered': {"$gt":"E"} })
+for i in record:
+    print(i)
